4992da8a45833b425cc48d03ae095417a3c4c96a
Abdul-Jabbar-txend <abdul.jabbar@txend.com>
2024-12-11 16:15:08 +0500

    updated new changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,5 +2,7 @@ exports.getProtectedData = (req, res) => {
   const role = req.user.role; // Extracted from the JWT token
   if (role === "admin") {
     return res.json({ message: "Hello Admin" });
+  } else {
+    return res.json({ message: "Hello User" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -8,28 +8,22 @@ const cors = require("cors"); // Import cors
 // Initialize express app
 const app = express();
 
-// Enable CORS for your frontend
-const corsOptions = {
-  origin: "http://localhost:5174", // Allow frontend to access backend
-  methods: "GET, POST", // Allow specific HTTP methods
-  allowedHeaders: "Content-Type, Authorization", // Allow specific headers
-};
+// const corsOptions = {
+//   origin: "http://localhost:5174", // Allow frontend to access backend
+//   methods: "GET, POST", // Allow specific HTTP methods
+//   allowedHeaders: "Content-Type, Authorization", // Allow specific headers
+// };
 
-// Use CORS with the specified options
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Load environment variables
 dotenv.config();
 
-// Connect to the database
 connectDB();
 
-// Routes
-app.use("/api/auth", authRoutes); // Authentication routes
-app.use("/api/user", userRoutes); // User routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/public", userRoutes);
 app.use("/api/protected", userRoutes);
 
